e72ffefa83dd6fa53d66d2818025ad522bc8175d
Alice Cecil <alycecil@gmail.com>
2018-09-29 21:40:23 -0400

    Pick Better

--- Source/mod/GeneSeedHediffWithComps.cs
+++ Source/mod/GeneSeedHediffWithComps.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using GeneSeed.Organs;
+using Harmony;
 using RimWorld;
 using Verse;
 
@@ -20,7 +21,40 @@ namespace GeneSeed
 
             RestoreOrgan();
 
+            DealWithMissingParts();
             ticks %= 3010349; //my favorite prime
+        }
+
+        private void DealWithMissingParts()
+        {
+            if (pawn.def != Constants.Astarte) return;
+            bool hadParts = false;
+            foreach (BodyPartDef astarteBodyPart in Constants.AstarteBodyParts)
+            {
+                if (hadParts) break;
+                foreach (var part in pawn.def.race.body.GetPartsWithDef(astarteBodyPart))
+                {
+                    hadParts = true;
+                    break;
+                }
+            }
+
+            if (hadParts) return;
+            Log.Message("[" + pawn + "] is missing the 19.");
+            foreach (BodyPartDef astarteBodyPart in Constants.AstarteBodyParts)
+            {
+                pawn.def.race.body.corePart.parts.Add(new BodyPartRecord
+                {
+                    coverage = 0.0001f,
+                    def = astarteBodyPart,
+                    depth = BodyPartDepth.Undefined,
+                    groups = new List<BodyPartGroupDef>(new[] {BodyPartGroupDefOf.Torso}),
+                    height = BodyPartHeight.Middle
+                });
+            }
+
+            pawn.def.race.body.ResolveReferences();
+            TransformPawn(false, true);
         }
 
         private void OrganFunctions()
@@ -54,7 +88,6 @@ namespace GeneSeed
             //time for a bonus organ
 
             bool didOne = false;
-
             foreach (BodyPartDef astarteBodyPart in Constants.AstarteBodyParts)
             {
                 if (didOne) break;
@@ -64,7 +97,6 @@ namespace GeneSeed
                     pawn.health.RestorePart(part);
 
                     didOne = true;
-                    break;
                 }
             }
 
@@ -72,16 +104,15 @@ namespace GeneSeed
             checkAllThatInsidesForGunk();
             this.Severity -= 0.1f;
         }
-
         public override void PostAdd(DamageInfo? dinfo)
         {
-            if (pawn.RaceProps.Humanlike && pawn.def == ThingDefOf.Human)
+            if (pawn.RaceProps.Humanlike && (pawn.def == ThingDefOf.Human || ThingDefOf.Human.race.body.defName == pawn.def.race.body.defName))
                 TransformPawn();
 
             base.PostAdd(dinfo);
         }
 
-        private void TransformPawn()
+        private void TransformPawn(bool changeDef = true, bool keep = false)
         {
             //Body change to Astarte
             var where = pawn.Position;
@@ -91,8 +122,7 @@ namespace GeneSeed
             pawn.DeSpawn();
             RegionListersUpdater.DeregisterInRegions(pawn, map);
 
-
-            if (Constants.Astarte != null)
+            if (changeDef && Constants.Astarte != null)
             {
                 pawn.def = Constants.Astarte;
             }
@@ -107,7 +137,7 @@ namespace GeneSeed
 
             //remove the 19
 
-            RemoveAstarteParts();
+            if (!keep) RemoveAstarteParts();
 
             //decache graphics
             pawn.Drawer.renderer.graphics.ResolveAllGraphics();
@@ -152,7 +182,8 @@ namespace GeneSeed
                 if (bodyPartRecords.Count() <= 1) continue;
                 ohHellNahWeAMutant = true;
                 Log.Message(
-                    "Uh Oh this Astarte is a damn mutant, they got more than one copy of the astarte organs ["+bodyPartRecords.First()+"]");
+                    "Uh Oh this Astarte is a damn mutant, they got more than one copy of the astarte organs [" +
+                    bodyPartRecords.First() + "]");
                 FilthMaker.MakeFilth(pawn.Position, pawn.Map, ThingDefOf.Filth_Blood, 6);
                 break;
             }
