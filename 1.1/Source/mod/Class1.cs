da195044318c1d53706861be0df00c1b1749a6c3
Ogliss <randomdragon2@hotmail.com>
2020-03-11 10:25:00 +0000

    HAR patches

---
+++ 1.1/Source/mod/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimWorld
+{
+    /*
+    [StaticConstructorOnStartup]
+    class GeneseedConstructor
+    {
+        static GeneseedConstructor()
+        {
+            Log.Message("Adding Adaptus Recipe");
+            var thingDef = DefDatabase<ThingDef>.GetNamed("AstarteAdaptusCore");
+
+
+            foreach (var recipe in ThingDefOf.Human.recipes)
+            {
+                Log.Message("Adding : " + recipe);
+                thingDef.recipes.Add(recipe);
+            }
+
+        }
+    }
+    */
+}
